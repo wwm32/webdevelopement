bad222ff4ce2227b27c3ff8401940c5e4075171b
FreeZ999-yur <wissem.moussa@student.vives.be>
2026-02-17 01:21:57 +0100

    yuh

---
+++ Labo_14/scripts/oefening_6.js
@@ -0,0 +1,19 @@
+const setup = () => {
+    let btnToon = document.getElementById("btnToon");
+    btnToon.addEventListener("click", toonSubstring);
+}
+
+const toonSubstring = () => {
+    let woord = document.getElementById("txtWoord").value;
+    let start = document.getElementById("txtStart").value;
+    let einde = document.getElementById("txtEinde").value;
+
+    start = parseInt(start);
+    einde = parseInt(einde);
+
+    let resultaat = woord.substring(start, einde);
+
+    document.getElementById("txtOutput").innerHTML = resultaat;
+}
+
+window.addEventListener("load", setup);
